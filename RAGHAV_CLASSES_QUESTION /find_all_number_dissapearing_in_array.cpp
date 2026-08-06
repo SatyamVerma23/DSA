52af0cf6fdc390d37d40bbbd917d0696ac58ae30
Satyam Verma <your-email@example.com>
2026-08-06 22:57:48 +0530

    finding missing number in array

---
+++ RAGHAV_CLASSES_QUESTION /find_all_number_dissapearing_in_array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+int main()
+{
+    int arr[]={4,3,2,7,8,2,3,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int i=0;
+    while(i<n)
+    {
+        int ci=arr[i]-1;
+        if(arr[i]==arr[ci]) i++;
+        else if(arr[i]==i+1)
+        {
+            i++;
+        }
+        else
+        {
+            swap(arr[i],arr[ci]);
+        }
+    }
+    vector<int>v;
+    int j=0;
+    while(j<n)
+    {
+        if(arr[j]==j+1) j++;
+        else
+        {
+            v.push_back(j+1);
+            j++;
+        }
+    }
+    int n2=v.size();
+    for (int i=0;i<n2;i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
