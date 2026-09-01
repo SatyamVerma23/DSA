bff9b85eb339fae7359525dbbb1b0cf8a365b512
Satyam Verma <satyam.kumar.verma2002@gmail.com>
2026-09-01 17:20:18 +0530

    add

---
+++ RAGHAV_CLASSES_QUESTION /making_target_value.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void combinations(vector<int>v,int arr[],int n,int target,int idx)
+{
+    if(target==0)
+    {
+        for (int i=0;i<v.size();i++)
+        {
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    if(target<0) return ;
+    for (int i=idx;i<n;i++)
+    {
+        v.push_back(arr[i]);
+        combinations(v,arr,n,target-arr[i],i);
+        v.pop_back();
+    }
+}
+int main()
+{
+    int arr[]={2,3,5};
+    int n=3;
+    int target =8;
+    vector<int>v;
+    combinations(v,arr,n,target,0);
+    return 0;
+}
