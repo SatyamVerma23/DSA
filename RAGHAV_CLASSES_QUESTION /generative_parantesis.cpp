76e5a04b09b7af7a30823ac733979e48733ccad7
Satyam Verma <satyam.kumar.verma2002@gmail.com>
2026-09-01 18:25:13 +0530

    add

---
+++ RAGHAV_CLASSES_QUESTION /generative_parantesis.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void generate(string s,int open,int close,int n)
+{
+    if(open==n && close==n)
+    {
+        cout<<s<<endl;
+        return;
+    }
+    if(open<n)
+    {
+        generate(s+'(',open+1,close,n);
+    }
+    if(open==close)
+    {
+        return;
+    }
+    else
+    {
+        generate(s+')',open,close+1,n);
+    }
+}
+int main()
+{
+    int n=3;
+    generate("",0,0,n);
+    return 0;
+}
