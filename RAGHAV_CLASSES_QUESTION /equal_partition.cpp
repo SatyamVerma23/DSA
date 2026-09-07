e5672c604f7b82828acf0c4bfcbcde80e2064d96
Satyam Verma <satyam.kumar.verma2002@gmail.com>
2026-09-07 23:40:23 +0530

    add

---
+++ RAGHAV_CLASSES_QUESTION /equal_partition.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5,5,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for (int i=1;i<n;i++)
+    {
+        arr[i]=arr[i]+arr[i-1];
+    }
+    int idx=-1;
+    bool flag=false;
+    for (int i=1;i<n;i++)
+    {
+        if(2*arr[i]==arr[n-1])
+        {
+            idx=i;
+            flag=true;
+        }
+    }
+    if(flag==true)
+    {
+        cout<<"partion is possible at "<<idx<<" index";
+    }
+}
