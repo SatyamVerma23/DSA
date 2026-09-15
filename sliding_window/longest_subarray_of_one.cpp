fb985dd57a94fcc53353598f2aea391ce5cd0005
Satyam Verma <satyam.kumar.verma2002@gmail.com>
2026-09-16 00:15:26 +0530

    add

---
+++ sliding_window/longest_subarray_of_one.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={0,1,1,1,0,1,1,0,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=1;
+    int i=0,j=0;
+    int l=INT_MIN,maxlen=INT_MIN;
+    int flip=0;
+    while(j<n)
+    {
+        if(arr[j]==1) 
+        {
+            j++;
+        }
+        else//(nums[j]==0)
+        {
+            if(flip<k)
+            {
+                flip++;
+                j++;//without make actual element  0 to 1(only consider in mind that we make jth elemnt to be 1)
+            }
+            else// flip==k
+            {
+                l=j-i; //cal lenght 
+                maxlen=max(maxlen,l);
+                while (arr[i]==1) i++; /// to shift i just befor first zero of that window
+
+                i++;
+                j++;
+            }
+        }
+    }
+    l=j-i;
+    maxlen=max(maxlen,l);
+    cout<<maxlen-1;
+    return 0;
+}
